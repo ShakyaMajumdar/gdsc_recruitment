0374ecb6f2a2b924c1c552ee98da0faab2fcc5f7
Shakya Majumdar <shakyamajumdar1@gmail.com>
2023-08-26 12:29:29 +0530

    add basic design

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
+import datetime
 from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     conf_resp = requests.get("https://gdscdev.vercel.app/api")
     conf_json = conf_resp.json()
     if not conf_json["status"]:
-        raise Exception 
+        raise Exception  # TODO
     return render_template("home.html", conferences=conf_json["content"]["data"])
+
+
+@app.template_filter()
+def format_datetime(dt):
+    return datetime.datetime.fromisoformat(dt).strftime("%d %B, %Y")
