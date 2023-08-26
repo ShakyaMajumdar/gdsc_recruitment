73ce9988c1aadedc25622e1c29cc1e2f2ba2ceea
Shakya Majumdar <shakyamajumdar1@gmail.com>
2023-08-26 15:05:51 +0530

    better error handling

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import datetime
 from flask import Flask, render_template
 import requests
+import werkzeug.exceptions
 
 app = Flask(__name__)
 
@@ -8,12 +9,16 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     conf_resp = requests.get("https://gdscdev.vercel.app/api")
-    conf_json = conf_resp.json()
-    if not conf_json["status"]:
-        raise Exception  # TODO
+    if not (conf_resp.ok and (conf_json := conf_resp.json())["status"]):
+        raise werkzeug.exceptions.BadGateway
     return render_template("home.html", conferences=conf_json["content"]["data"])
 
 
 @app.template_filter()
 def format_datetime(dt):
     return datetime.datetime.fromisoformat(dt).strftime("%d %B, %Y")
+
+
+@app.errorhandler(werkzeug.exceptions.BadGateway)
+def handle_bad_gateway(e):
+    return render_template("502.html"), 502
